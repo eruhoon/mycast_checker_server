0c0916c383120b450698f688daa9d9433067c6c9
엘풍 <eruhoon@gmail.com>
2018-08-03 01:20:26 +0900

    mask stream uri

--- src/controller/StreamRewardProvider.ts
+++ src/controller/StreamRewardProvider.ts
@@ -4,7 +4,9 @@ export class StreamRewardProvider {
 
 	public requestStreamReward(hash: string, reward: number) {
 		console.log('StreamRewardProvider#requestStreamReward: ', hash, reward);
-		let url = `http://mycast.xyz:9020/${hash}/reward/stream`;
+		let host = process.env.SHOP_SERVER_HOST;
+		let port = process.env.SHOP_SERVER_PORT;
+		let url = `http://${host}:${port}/${hash}/reward/stream`;
 		let form = { reward };
 		let opt = { timeout: 5000, form };
 		request.post(url, opt);
