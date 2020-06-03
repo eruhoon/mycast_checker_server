f5c11ad3cc032dd12d35f40d724e6518b3b5ba24
엘풍 <eruhoon@gmail.com>
2020-06-03 22:32:51 +0900

    fix lint

--- src/StreamCheckerServer.ts
+++ src/StreamCheckerServer.ts
@@ -11,60 +11,60 @@ import { SocketManager, SocketTag } from './SocketManager';
 
 export class StreamCheckerServer {
 
-	public static main() {
+    public static main() {
 
-		const serverManager: ServerManager = ServerManager.getInstance();
+        const serverManager: ServerManager = ServerManager.getInstance();
 
-		let userLoader: IUserAsyncLoader;
-		let streamLoader: IStreamAsyncLoader;
-		if (Config.isDebugMode()) {
-			userLoader = new DummyUserAsyncLoader();
-			streamLoader = new DummyStreamAsyncLoader();
-		} else {
-			const databaseLoader = new DatabaseLoader();
-			userLoader = databaseLoader;
-			streamLoader = databaseLoader;
-		}
+        let userLoader: IUserAsyncLoader;
+        let streamLoader: IStreamAsyncLoader;
+        if (Config.isDebugMode()) {
+            userLoader = new DummyUserAsyncLoader();
+            streamLoader = new DummyStreamAsyncLoader();
+        } else {
+            const databaseLoader = new DatabaseLoader();
+            userLoader = databaseLoader;
+            streamLoader = databaseLoader;
+        }
 
-		const checker = new Checker(userLoader, streamLoader);
+        const checker = new Checker(userLoader, streamLoader);
 
-		serverManager.get('/stream/', (req, res) => {
-			let streams = checker.getStreams();
-			res.json(streams);
-		});
+        serverManager.get('/stream/', (req, res) => {
+            const streams = checker.getStreams();
+            res.json(streams);
+        });
 
-		serverManager.get('/local/', (req, res) => {
-			let streams = checker.getStreams().local;
-			res.json(streams);
-		});
+        serverManager.get('/local/', (req, res) => {
+            const streams = checker.getStreams().local;
+            res.json(streams);
+        });
 
-		serverManager.get('/external/', (req, res) => {
-			let streams = checker.getStreams().external;
-			res.json(streams);
-		});
+        serverManager.get('/external/', (req, res) => {
+            const streams = checker.getStreams().external;
+            res.json(streams);
+        });
 
-		serverManager.get('/twitch/', (req, res) => {
-			let streams = checker.getStreams().external.filter((e) => {
-				return e.platform === StreamPlatform.TWITCH;
-			});
-			res.json(streams);
-		});
+        serverManager.get('/twitch/', (req, res) => {
+            const streams = checker.getStreams().external.filter((e) => {
+                return e.platform === StreamPlatform.TWITCH;
+            });
+            res.json(streams);
+        });
 
-		serverManager.start();
+        serverManager.start();
 
-		let server = serverManager.getServer();
+        const server = serverManager.getServer();
 
-		let socketManager = new SocketManager(server, userLoader);
-		socketManager.init(socket => {
-			socket.emit(SocketTag.REFRESH_STREAMS, checker.getStreams());
-		});
+        const socketManager = new SocketManager(server, userLoader);
+        socketManager.init(socket => {
+            socket.emit(SocketTag.REFRESH_STREAMS, checker.getStreams());
+        });
 
-		checker.update();
-		socketManager.refreshStreams(checker.getStreams());
-		setInterval(() => {
-			checker.update();
-			socketManager.refreshStreams(checker.getStreams());
-		}, 10000);
+        checker.update();
+        socketManager.refreshStreams(checker.getStreams());
+        setInterval(() => {
+            checker.update();
+            socketManager.refreshStreams(checker.getStreams());
+        }, 10000);
 
-	}
+    }
 }
