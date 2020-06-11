298e4f546864fb53584f7bf89a95eb956d3a442b
엘풍 <eruhoon@gmail.com>
2020-06-11 22:18:25 +0900

    fix lint

--- src/streamloader/TwitchLoader.ts
+++ src/streamloader/TwitchLoader.ts
@@ -4,38 +4,38 @@ import { StreamLoader, StreamLoaderCallback } from './StreamLoader';
 
 export class TwtichLoader extends StreamLoader {
 
-	private mManager: TwitchCacheContainer;
-	private mKeyword: string;
+    private mManager: TwitchCacheContainer;
+    private mKeyword: string;
 
-	public constructor(manager: TwitchCacheContainer, keyword: string) {
-		super();
-		this.mManager = manager;
-		this.mKeyword = keyword;
-	}
+    public constructor(manager: TwitchCacheContainer, keyword: string) {
+        super();
+        this.mManager = manager;
+        this.mKeyword = keyword;
+    }
 
-	public requestInfo(callback: StreamLoaderCallback) {
-		let cache = this.mManager.getCache(this.mKeyword);
-		if (cache === null) {
-			return;
-		}
-		if (cache.stream === null) {
-			return;
-		}
+    public requestInfo(callback: StreamLoaderCallback) {
+        let cache = this.mManager.getCache(this.mKeyword);
+        if (cache === null) {
+            return;
+        }
+        if (cache.stream === null) {
+            return;
+        }
 
-		let info: StreamInfo = {
-			result: true,
-			platform: StreamPlatform.TWITCH,
-			keyid: cache.keyword,
-			icon: cache.user.profile_image_url,
-			nickname: cache.user.display_name,
-			title: cache.user.display_name,
-			description: cache.stream.title,
-			url: `https://player.twitch.tv/?channel=${this.mKeyword}`,
-			onair: true,
-			viewer: cache.stream.viewer_count,
-			thumbnail: cache.stream.thumbnail_url
-		};
-		callback(info);
-	}
+        let info: StreamInfo = {
+            result: true,
+            platform: StreamPlatform.TWITCH,
+            keyid: cache.keyword,
+            icon: cache.user.profile_image_url,
+            nickname: cache.user.display_name,
+            title: cache.user.display_name,
+            description: cache.stream.title,
+            url: `https://player.twitch.tv/?channel=${this.mKeyword}`,
+            onair: true,
+            viewer: cache.stream.viewer_count,
+            thumbnail: cache.stream.thumbnail_url
+        };
+        callback(info);
+    }
 
 }
