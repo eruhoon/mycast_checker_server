4beaeb92fbb4569644abfbe0f02f1aac9225f40b
엘풍 <eruhoon@gmail.com>
2020-09-28 22:45:02 +0900

    fix lint

--- src/model/cache/TwitchCacheContainer.ts
+++ src/model/cache/TwitchCacheContainer.ts
@@ -1,104 +1,122 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
-import { IStreamAsyncLoader } from '../../controller/IStreamAsyncLoader';
-import { IUserAsyncLoader } from '../../controller/IUserAsyncLoader';
-import { StreamPlatform } from '../../model/Stream';
-import { TwitchStreamCache } from '../../model/TwitchStreamCache';
-import { RawTwitchStream, RawTwitchUser, TwitchUtils } from '../../utils/TwitchUtils';
-import { StreamCacheContainer } from './StreamCacheContainer';
+import { IStreamAsyncLoader } from "../../controller/IStreamAsyncLoader";
+import { IUserAsyncLoader } from "../../controller/IUserAsyncLoader";
+import { StreamPlatform } from "../../model/Stream";
+import { TwitchStreamCache } from "../../model/TwitchStreamCache";
+import {
+    RawTwitchStream,
+    RawTwitchUser,
+    TwitchUtils,
+} from "../../utils/TwitchUtils";
+import { StreamCacheContainer } from "./StreamCacheContainer";
 
 export class TwitchCacheContainer extends StreamCacheContainer {
+    private mCaches: TwitchStreamCache[];
+    private mNewCaches: TwitchStreamCache[];
+    private mUserLoader: IUserAsyncLoader;
+    private mStreamLoader: IStreamAsyncLoader;
 
-	private mCaches: TwitchStreamCache[];
-	private mNewCaches: TwitchStreamCache[];
-	private mUserLoader: IUserAsyncLoader;
-	private mStreamLoader: IStreamAsyncLoader;
+    public constructor(
+        userLoader: IUserAsyncLoader,
+        streamloader: IStreamAsyncLoader
+    ) {
+        super();
+        dotenv.config();
+        this.mCaches = [];
 
-	public constructor(
-		userLoader: IUserAsyncLoader, streamloader: IStreamAsyncLoader) {
+        this.mUserLoader = userLoader;
+        this.mStreamLoader = streamloader;
+    }
 
-		super();
-		dotenv.config();
-		this.mCaches = [];
+    public async update() {
+        console.time("TwitchCacheContainer#update");
 
-		this.mUserLoader = userLoader;
-		this.mStreamLoader = streamloader;
-	}
+        if (this.mUserLoader === null || this.mStreamLoader === null) {
+            console.error("TwitchCacheContainer#update: no loader");
+        } else {
+            const keywordsFromUser: string[] = (
+                await this.mUserLoader.getUsers()
+            )
+                .filter((u) => u.getStreamPlatform() === StreamPlatform.TWITCH)
+                .map((u) => u.getStreamKeyId());
 
-	public async update() {
-		console.time('TwitchCacheContainer#update');
+            const keywordsFromStream: string[] = (
+                await this.mStreamLoader.getStreams()
+            )
+                .filter((stream) => stream.platform === StreamPlatform.TWITCH)
+                .map((stream) => stream.keyword);
 
-		if (this.mUserLoader === null || this.mStreamLoader === null) {
-			console.error('TwitchCacheContainer#update: no loader');
-		} else {
-			let keywordsFromUser: string[] = (await this.mUserLoader.getUsers())
-				.filter(u => u.getStreamPlatform() === StreamPlatform.TWITCH)
-				.map(u => u.getStreamKeyId());
+            const keywords: string[] = [];
+            const addWithoutDuplicated = (keyword: string) => {
+                if (!keyword) {
+                    return;
+                }
+                if (keywords.findIndex((k) => k === keyword) !== -1) {
+                    return;
+                }
+                keywords.push(keyword);
+            };
+            keywordsFromUser.forEach((keyword) =>
+                addWithoutDuplicated(keyword)
+            );
+            keywordsFromStream.forEach((keyword) =>
+                addWithoutDuplicated(keyword)
+            );
 
-			let keywordsFromStream: string[] = (await this.mStreamLoader.getStreams())
-				.filter(stream => stream.platform === StreamPlatform.TWITCH)
-				.map(stream => stream.keyword);
+            this.applyKeywords(keywords);
+            const userData = await TwitchUtils.loadUser(keywords);
+            this.applyUserInfos(userData);
+            const streams = await TwitchUtils.loadStream(
+                userData.map((u) => u.login)
+            );
+            this.applyStreamInfos(streams);
+            this.mCaches = this.mNewCaches;
 
-			let keywords: string[] = [];
-			let addWithoutDuplicated = (keyword: string) => {
-				if (!keyword) return;
-				if (keywords.findIndex(k => k === keyword) !== -1) return;
-				keywords.push(keyword);
-			};
-			keywordsFromUser.forEach(keyword => addWithoutDuplicated(keyword));
-			keywordsFromStream.forEach(keyword => addWithoutDuplicated(keyword));
+            const total = this.mCaches.length;
+            const onair = this.mCaches.filter((cache) => cache.stream != null)
+                .length;
+            console.log(`TwitchCacheContainer#update: ${onair}/${total}`);
+        }
+        console.timeEnd("TwitchCacheContainer#update");
+    }
 
-			this.applyKeywords(keywords);
-			let userData = await TwitchUtils.loadUser(keywords);
-			this.applyUserInfos(userData);
-			let streams = await TwitchUtils.loadStream(userData.map(u => u.login));
-			this.applyStreamInfos(streams);
-			this.mCaches = this.mNewCaches;
+    public getCache(keyword: string): TwitchStreamCache | null {
+        const cache = this.mCaches.find((cache) => cache.keyword === keyword);
+        if (!cache) {
+            return null;
+        }
+        return cache;
+    }
 
-			let total = this.mCaches.length;
-			let onair = this.mCaches.filter(cache => cache.stream != null).length;
-			console.log(`TwitchCacheContainer#update: ${onair}/${total}`);
+    private applyKeywords(keywords: string[]) {
+        this.mNewCaches = keywords.map((keyword) => {
+            return { keyword, user: null, stream: null };
+        });
+    }
 
-		}
-		console.timeEnd('TwitchCacheContainer#update');
-	}
+    private applyUserInfos(users: RawTwitchUser[]) {
+        users.forEach((user) => {
+            const cache = this.mNewCaches.find(
+                (cache) => user.login === cache.keyword
+            );
+            if (cache) {
+                cache.user = user;
+            }
+        });
+    }
 
-	public getCache(keyword: string): TwitchStreamCache | null {
-		let cache = this.mCaches.find(cache => cache.keyword === keyword);
-		if (!cache) {
-			return null;
-		}
-		return cache;
-	}
-
-	private applyKeywords(keywords: string[]) {
-		this.mNewCaches = keywords.map(keyword => {
-			return { keyword, user: null, stream: null };
-		});
-	}
-
-	private applyUserInfos(users: RawTwitchUser[]) {
-		users.forEach(user => {
-			const cache = this.mNewCaches.find(
-				cache => user.login === cache.keyword);
-			if (cache) {
-				cache.user = user;
-			}
-		});
-	}
-
-	private applyStreamInfos(streams: RawTwitchStream[]) {
-		streams.forEach(stream => {
-			const cache = this.mNewCaches.find(cache => {
-				if (!cache.user) {
-					return false;
-				}
-				return cache.user.id === stream.user_id;
-			});
-			if (cache) {
-				cache.stream = stream;
-			}
-		});
-	}
-
+    private applyStreamInfos(streams: RawTwitchStream[]) {
+        streams.forEach((stream) => {
+            const cache = this.mNewCaches.find((cache) => {
+                if (!cache.user) {
+                    return false;
+                }
+                return cache.user.id === stream.user_id;
+            });
+            if (cache) {
+                cache.stream = stream;
+            }
+        });
+    }
 }
--- src/utils/TwitchUtils.ts
+++ src/utils/TwitchUtils.ts
@@ -1,58 +1,57 @@
-import Axios from 'axios';
-import * as qs from 'querystring';
-import { ArrayUtils } from '../model/common/array/ArrayUtils';
-import { Logger } from '../model/common/logger/Logger';
+import Axios from "axios";
+import * as qs from "querystring";
+import { ArrayUtils } from "../model/common/array/ArrayUtils";
+import { Logger } from "../model/common/logger/Logger";
 
 export type RawTwitchUser = {
-    broadcaster_type: RawTwitchBroadcastType,
-    description: string,
-    display_name: string,
-    email: string,
-    id: string,
-    login: string,
-    offline_image_url: string,
-    profile_image_url: string,
-    type: RawTwitchUserType,
-    view_count: number,
+    broadcaster_type: RawTwitchBroadcastType;
+    description: string;
+    display_name: string;
+    email: string;
+    id: string;
+    login: string;
+    offline_image_url: string;
+    profile_image_url: string;
+    type: RawTwitchUserType;
+    view_count: number;
 };
 
 export type RawTwitchStream = {
-    community_ids: string[],
-    game_id: string,
-    id: string,
-    language: string,
-    pagination: string,
-    started_at: string,
-    thumbnail_url: string,
-    title: string,
-    type: string,
-    user_id: string,
-    viewer_count: number,
+    community_ids: string[];
+    game_id: string;
+    id: string;
+    language: string;
+    pagination: string;
+    started_at: string;
+    thumbnail_url: string;
+    title: string;
+    type: string;
+    user_id: string;
+    viewer_count: number;
 };
 
-type RawTwitchBroadcastType = 'parter' | 'affiliate' | '';
+type RawTwitchBroadcastType = "parter" | "affiliate" | "";
 
-type RawTwitchUserType = 'staff' | 'admin' | 'global_mod' | '';
+type RawTwitchUserType = "staff" | "admin" | "global_mod" | "";
 
 export class TwitchUtils {
-
-    private static readonly sLogger = new Logger('TwitchUtils');
+    private static readonly sLogger = new Logger("TwitchUtils");
 
     public static async loadUser(loginIds: string[]): Promise<RawTwitchUser[]> {
         const accessToken = await this.getAccessToken();
         if (!accessToken) {
-            this.sLogger.error('Invalid accessToken');
+            this.sLogger.error("Invalid accessToken");
             return [];
         }
-        const host = 'https://api.twitch.tv/helix/users';
-        const query = loginIds.map(k => `login=${k}`).join('&');
+        const host = "https://api.twitch.tv/helix/users";
+        const query = loginIds.map((k) => `login=${k}`).join("&");
         const url = `${host}?${query}`;
         try {
             const res = await Axios.get(url, {
                 timeout: 5000,
                 headers: {
                     Authorization: `Bearer ${accessToken}`,
-                    'Client-ID': process.env.TWITCH_CLIENT_ID,
+                    "Client-ID": process.env.TWITCH_CLIENT_ID,
                 },
             });
             const users: RawTwitchUser[] = res.data.data;
@@ -63,10 +62,12 @@ export class TwitchUtils {
         }
     }
 
-    public static async loadStream(keywords: string[]): Promise<RawTwitchStream[]> {
+    public static async loadStream(
+        keywords: string[]
+    ): Promise<RawTwitchStream[]> {
         const accessToken = await this.getAccessToken();
         if (!accessToken) {
-            this.sLogger.error('Invalid accessToken');
+            this.sLogger.error("Invalid accessToken");
             return [];
         }
         const keywordChunks = ArrayUtils.chunk<string>(keywords, 100);
@@ -80,11 +81,12 @@ export class TwitchUtils {
     }
 
     private static async loadStreamInternal(
-        keywords: string[], token: string): Promise<RawTwitchStream[]> {
-
-        const userQuery = keywords.map(k => `user_login=${k}`).join('&');
+        keywords: string[],
+        token: string
+    ): Promise<RawTwitchStream[]> {
+        const userQuery = keywords.map((k) => `user_login=${k}`).join("&");
         const query = `${userQuery}&first=100`;
-        const host = 'https://api.twitch.tv/helix/streams';
+        const host = "https://api.twitch.tv/helix/streams";
         const url = `${host}?${query}`;
 
         try {
@@ -92,11 +94,11 @@ export class TwitchUtils {
                 timeout: 5000,
                 headers: {
                     Authorization: `Bearer ${token}`,
-                    'Client-ID': process.env.TWITCH_CLIENT_ID,
+                    "Client-ID": process.env.TWITCH_CLIENT_ID,
                 },
             });
             const streams: RawTwitchStream[] = res.data.data;
-            streams.forEach(s => {
+            streams.forEach((s) => {
                 const format = s.thumbnail_url;
                 s.thumbnail_url = this.decorateThumbnail(format, 200, 150);
             });
@@ -105,16 +107,15 @@ export class TwitchUtils {
             this.sLogger.error(`TwitchUtils#loadStream: Request Error: ${e}`);
             return [];
         }
-
     }
 
     private static async getAccessToken(): Promise<string | null> {
-        const host = 'https://id.twitch.tv/oauth2/token';
+        const host = "https://id.twitch.tv/oauth2/token";
         const query = qs.stringify({
             client_id: process.env.TWITCH_CLIENT_ID,
             client_secret: process.env.TWITCH_SECRET,
-            grant_type: 'client_credentials',
-            scope: 'user:read:email',
+            grant_type: "client_credentials",
+            scope: "user:read:email",
         });
         const url = `${host}?${query}`;
         try {
@@ -127,11 +128,14 @@ export class TwitchUtils {
     }
 
     private static decorateThumbnail(
-        format: string, width: number, height: number): string {
-        let result = format.replace('{width}', width.toString())
-            .replace('{height}', height.toString());
-        result += '?' + new Date().getTime();
+        format: string,
+        width: number,
+        height: number
+    ): string {
+        let result = format
+            .replace("{width}", width.toString())
+            .replace("{height}", height.toString());
+        result += "?" + new Date().getTime();
         return result;
-
     }
 }
