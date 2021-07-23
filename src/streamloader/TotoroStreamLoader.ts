7aa5a1144f530d7ba4d3576438a7d553c3a1231a
Whimsicott <eruhoon@gmail.com>
2021-07-24 00:36:22 +0900

    update TotoroStreamLoader

--- src/streamloader/TotoroStreamLoader.ts
+++ src/streamloader/TotoroStreamLoader.ts
@@ -1,44 +1,45 @@
 import { TotoroCacheContainer } from '../model/cache/TotoroCacheContainer';
 import { StreamInfo, StreamPlatform } from '../model/Stream';
 import { User } from '../model/User';
-import { StreamLoader, StreamLoaderCallback } from './StreamLoader';
+import { StreamLoader2 } from './StreamLoader2';
 
-export class TotoroStreamLoader extends StreamLoader {
-  mManager: TotoroCacheContainer;
-  mUser: User;
+export class TotoroStreamLoader extends StreamLoader2 {
+  #manager: TotoroCacheContainer;
+  #user: User;
 
   constructor(manager: TotoroCacheContainer, user: User) {
     super();
-    this.mManager = manager;
-    this.mUser = user;
+    this.#manager = manager;
+    this.#user = user;
   }
 
-  requestInfo(callback: StreamLoaderCallback): void {
-    const caches = this.mManager.getCaches();
-    const streamKey = this.mUser.getStreamKeyId();
+  async getInfo(): Promise<StreamInfo | null> {
+    const caches = this.#manager.getCaches();
+    const streamKey = this.#user.getStreamKeyId();
 
     const cache = caches.find((cache) => cache.name === streamKey);
-    if (cache) {
-      const nickname = this.mUser.getNickname();
-      const info: StreamInfo = {
-        result: true,
-        platform: StreamPlatform.TOTORO,
-        keyid: streamKey,
-        icon: this.mUser.getIcon(),
-        nickname: this.mUser.getNickname(),
-        // thumbnail: this.mUser.getBackground(), //2dfff.png
-        thumbnail: this.getThumbnail(streamKey),
-        onair: true,
-        title: nickname,
-        description: `${nickname}의 방송 [이웃채널]`,
-        url: `https://mycast.xyz/player/totoro/${streamKey}`,
-        viewer: cache.clients,
-      };
-      callback(info);
+    if (!cache) {
+      return null;
     }
+    const nickname = this.#user.getNickname();
+    const info: StreamInfo = {
+      result: true,
+      platform: StreamPlatform.TOTORO,
+      keyid: streamKey,
+      icon: this.#user.getIcon(),
+      nickname: this.#user.getNickname(),
+      // thumbnail: this.mUser.getBackground(), //2dfff.png
+      thumbnail: this.#getThumbnail(streamKey),
+      onair: true,
+      title: nickname,
+      description: `${nickname}의 방송 [이웃채널]`,
+      url: `https://mycast.xyz/player/totoro/${streamKey}`,
+      viewer: cache.clients,
+    };
+    return info;
   }
 
-  private getThumbnail(streamKey: string): string {
+  #getThumbnail(streamKey: string): string {
     const host = 'https://parasite.banjai.tv/live';
     const fileName = `${streamKey}.jpeg`;
     const timestamp = new Date().getTime();
