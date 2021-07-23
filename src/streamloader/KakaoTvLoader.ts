fac294145b1ebc43fc6fe8d512d775c50301fb00
Whimsicott <eruhoon@gmail.com>
2021-07-23 23:40:35 +0900

    update KakaoTvLoader

--- src/streamloader/KakaoTvLoader.ts
+++ src/streamloader/KakaoTvLoader.ts
@@ -1,6 +1,4 @@
-import * as request from 'request';
-import { callbackify } from 'util';
-
+import axios from 'axios';
 import { StreamInfo, StreamPlatform } from '../model/Stream';
 import { StreamLoader, StreamLoaderCallback } from './StreamLoader';
 
@@ -20,22 +18,30 @@ type RawKakaoTvVideo = {
 };
 
 export class KakaoTvLoader extends StreamLoader {
-  mChannelId: string;
+  #channelId: string;
 
   constructor(id: string) {
     super();
-    this.mChannelId = id;
+    this.#channelId = id;
   }
 
   requestInfo(callback: StreamLoaderCallback): void {
-    KakaoTvLoader.loadInfo(this.mChannelId, callback);
+    KakaoTvLoader.#loadInfo(this.#channelId).then((stream) => {
+      if (stream) {
+        callback(stream);
+      }
+    });
   }
-  private static async loadInfo(
-    channelId: string,
-    callback: StreamLoaderCallback
-  ) {
-    const channel = await this.loadChannel(channelId);
-    const video = await this.loadVideo(channel.videoId);
+
+  static async #loadInfo(channelId: string): Promise<StreamInfo | null> {
+    const channel = await this.#loadChannel(channelId);
+    if (!channel) {
+      return null;
+    }
+    const video = await this.#loadVideo(channel.videoId);
+    if (!video) {
+      return null;
+    }
     const stream: StreamInfo = {
       result: true,
       platform: StreamPlatform.KAKAOTV,
@@ -49,77 +55,65 @@ export class KakaoTvLoader extends StreamLoader {
       url: video.url,
       viewer: video.viewer,
     };
-    callback(stream);
+    return stream;
   }
 
-  private static loadChannel(channelId: string): Promise<RawKakaoTvChannel> {
-    return new Promise((resolve, reject) => {
-      const opt = {
-        url: `http://web-tv.kakao.com/channel/${channelId}`,
-        timeout: 5000,
-      };
+  static async #loadChannel(
+    channelId: string
+  ): Promise<RawKakaoTvChannel | null> {
+    const url = `http://web-tv.kakao.com/channel/${channelId}`;
+    const res = await axios.get(url, { timeout: 5000 });
+    const body = res.data;
+    if (res.status !== 200) {
+      return null;
+    }
 
-      request.get(opt, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          return;
-        }
+    const path = res.request.uri.pathname;
+    const isLive = path.indexOf('livelink') !== -1;
+    if (!isLive) {
+      return null;
+    }
 
-        const path = res.request.uri.pathname;
-        const isLive = path.indexOf('livelink') !== -1;
-        if (!isLive) {
-          return;
-        }
-
-        const matched = path.match(/livelink\/(.*)/);
-        if (!matched) {
-          return;
-        }
-        const videoId = matched[1];
-
-        const icon = body.match(/\<meta.*og\:image.*content=\"(.*)\"\>/)[1];
-
-        const channel: RawKakaoTvChannel = {
-          id: channelId,
-          icon,
-          videoId,
-        };
-        resolve(channel);
-      });
-    });
+    const matched = path.match(/livelink\/(.*)/);
+    if (!matched) {
+      return null;
+    }
+    const videoId = matched[1];
+    const icon = body.match(/\<meta.*og\:image.*content=\"(.*)\"\>/)[1];
+    const channel = { id: channelId, icon, videoId };
+    return channel;
   }
 
-  private static loadVideo(videoId: string): Promise<RawKakaoTvVideo> {
+  static async #loadVideo(videoId: string): Promise<RawKakaoTvVideo | null> {
     const url = `http://web-tv.kakao.com/api/v1/app/livelinks/${videoId}/impress?fulllevels=liveLink&player=monet_flash&section=home&dteType=PC&service=kakao_tv&fields=ccuCount,thumbnailUri`;
 
-    return new Promise((resolve) => {
-      const opt = { timeout: 5000, json: true };
-      request.get(url, opt, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          return;
-        }
+    const opt = { timeout: 5000 };
+    const res = await axios.get(url, opt);
+    const body = res.data;
+    if (res.status !== 200) {
+      return null;
+    }
 
-        if (!body || !body.liveLink) {
-          return;
-        }
+    if (!body || !body.liveLink) {
+      return null;
+    }
 
-        const channel = body.liveLink.channel;
-        const live = body.liveLink.live;
+    const channel = body.liveLink.channel;
+    const live = body.liveLink.live;
 
-        const isOnAir = live.status === 'ONAIR';
-        if (!isOnAir) {
-          return;
-        }
+    const isOnAir = live.status === 'ONAIR';
+    if (!isOnAir) {
+      return null;
+    }
 
-        const video: RawKakaoTvVideo = {
-          nickname: channel.name,
-          title: channel.name,
-          description: live.title,
-          url: `http://web-tv.kakao.com//embed/player/livelink/${videoId}`,
-          thumbnail: live.thumbnailUri,
-          viewer: live.ccuCount,
-        };
-        resolve(video);
-      });
-    });
+    const video: RawKakaoTvVideo = {
+      nickname: channel.name,
+      title: channel.name,
+      description: live.title,
+      url: `http://web-tv.kakao.com//embed/player/livelink/${videoId}`,
+      thumbnail: live.thumbnailUri,
+      viewer: live.ccuCount,
+    };
+    return video;
   }
 }
