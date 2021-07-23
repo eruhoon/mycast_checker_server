931a917c411e0d93f61e796764295c367163149e
Whimsicott <eruhoon@gmail.com>
2021-07-24 00:33:29 +0900

    update NewLocalStreamLoader

--- src/streamloader/NewLocalStreamLoader.ts
+++ src/streamloader/NewLocalStreamLoader.ts
@@ -1,43 +1,44 @@
 import { NewLocalCacheContainer } from '../model/cache/NewLocalCacheContainer';
 import { StreamInfo, StreamPlatform } from '../model/Stream';
 import { User } from '../model/User';
-import { StreamLoader, StreamLoaderCallback } from './StreamLoader';
+import { StreamLoader2 } from './StreamLoader2';
 
-export class NewLocalStreamLoader extends StreamLoader {
-  mManager: NewLocalCacheContainer;
-  mUser: User;
+export class NewLocalStreamLoader extends StreamLoader2 {
+  #manager: NewLocalCacheContainer;
+  #user: User;
 
   constructor(manager: NewLocalCacheContainer, user: User) {
     super();
-    this.mManager = manager;
-    this.mUser = user;
+    this.#manager = manager;
+    this.#user = user;
   }
 
-  requestInfo(callback: StreamLoaderCallback): void {
-    const caches = this.mManager.getCaches();
-    const streamKey = this.mUser.getStreamKeyId();
+  async getInfo(): Promise<StreamInfo | null> {
+    const caches = this.#manager.getCaches();
+    const streamKey = this.#user.getStreamKeyId();
 
     const cache = caches.find((cache) => cache.name === streamKey);
-    if (cache) {
-      const nickname = this.mUser.getNickname();
-      const info: StreamInfo = {
-        result: true,
-        platform: StreamPlatform.LOCAL,
-        keyid: streamKey,
-        icon: this.mUser.getIcon(),
-        nickname: this.mUser.getNickname(),
-        thumbnail: this.getThumbnail(streamKey),
-        onair: true,
-        title: nickname,
-        description: `${nickname}의 방송 [공용채널]`,
-        url: `https://mycast.xyz/player/${streamKey}`,
-        viewer: cache.clients,
-      };
-      callback(info);
+    if (!cache) {
+      return null;
     }
+    const nickname = this.#user.getNickname();
+    const info: StreamInfo = {
+      result: true,
+      platform: StreamPlatform.LOCAL,
+      keyid: streamKey,
+      icon: this.#user.getIcon(),
+      nickname: this.#user.getNickname(),
+      thumbnail: this.#getThumbnail(streamKey),
+      onair: true,
+      title: nickname,
+      description: `${nickname}의 방송 [공용채널]`,
+      url: `https://mycast.xyz/player/${streamKey}`,
+      viewer: cache.clients,
+    };
+    return info;
   }
 
-  private getThumbnail(streamKey: string): string {
+  #getThumbnail(streamKey: string): string {
     const host = 'https://mycast.xyz:9011/thumbs';
     const fileName = `${streamKey}-001.png`;
     const timestamp = new Date().getTime();
