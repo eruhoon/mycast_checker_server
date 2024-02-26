91ec853bfd3a571e40743387cd42d0b3a7d5796c
엘풍 <eruhoon@gmail.com>
2024-02-26 23:46:57 +0900

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { StreamCheckerServer } from './StreamCheckerServer';
 import { DiscordApp } from './app/discord/DiscordApp';
 
-StreamCheckerServer.main();
-
 new DiscordApp().main();
+StreamCheckerServer.main();
