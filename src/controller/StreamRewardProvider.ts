d11fad7627f54c3c182a02dbd0b4e0e5d58bdeab
엘풍 <eruhoon@gmail.com>
2024-02-28 20:28:04 +0900

    remove request

--- src/controller/StreamRewardProvider.ts
+++ src/controller/StreamRewardProvider.ts
@@ -1,21 +1,22 @@
-import * as request from 'request';
 import { Logger } from '../model/common/logger/Logger';
+import axios from 'axios';
 
 export class StreamRewardProvider {
   #logger = new Logger('StreamRewardProvider');
 
-  requestStreamReward(hash: string, reward: number) {
+  async requestStreamReward(hash: string, reward: number) {
     const host = process.env.SHOP_SERVER_HOST;
     const port = process.env.SHOP_SERVER_PORT;
     const url = `http://${host}:${port}/${hash}/reward/stream`;
     const form = { reward };
     const opt = { timeout: 5000, form };
-    request.post(url, opt, (err, res, body) => {
-      if (err || res.statusCode !== 200) {
-        this.#logger.log(`requestStreamReward: ${hash}/${reward}: FAILED`);
-      } else {
+    try {
+      const { status } = await axios.post(url, opt);
+      if (status !== 200) {
         this.#logger.log(`requestStreamReward: ${hash}/${reward}: SUCCESS`);
       }
-    });
+    } catch {
+      this.#logger.log(`requestStreamReward: ${hash}/${reward}: FAILED`);
+    }
   }
 }
