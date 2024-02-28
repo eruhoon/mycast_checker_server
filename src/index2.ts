7fbbad5dcf0c4c9c0571995a851afdb6b61d65ea
엘풍 <eruhoon@gmail.com>
2024-02-28 22:17:32 +0900

    update bot

---
+++ src/index2.ts
@@ -0,0 +1,14 @@
+//https://youtu.be/wFIQZLSuqjk
+
+import { YoutubeCacheContainer } from './model/cache/YoutubeCacheContainer';
+import { YoutubeHandleLoader } from './streamloader/YoutubeHandleLoader';
+import { YoutubeVideoLoader } from './streamloader/YoutubeVideoLoader';
+
+// new YoutubeVideoLoader('wFIQZLSuqjk').getInfo2();
+
+new YoutubeHandleLoader(
+  new YoutubeCacheContainer(),
+  '@OtonoseKanade'
+).getInfo();
+
+647018 + 750405;
